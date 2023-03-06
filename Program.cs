630627049d5fdda15a38f7511d3ccfcc82969e7c
ThibautJoncour <96741150+ThibautJoncour@users.noreply.github.com>
2023-03-06 11:18:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessStartInfo start = new ProcessStartInfo();
+        start.FileName = "python";
+        start.Arguments = "C:\\Users\\tibo9\\source\\repos\\PythonApplication1\\PythonApplication1\\Test.py";
+        start.UseShellExecute = false;
+        start.RedirectStandardOutput = true;
+
+        using (Process process = Process.Start(start))
+        {
+            using (StreamReader reader = process.StandardOutput)
+            {
+                string result = reader.ReadToEnd();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
